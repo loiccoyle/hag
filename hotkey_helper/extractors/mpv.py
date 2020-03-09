f3d03f9be41b4695f9a8786cdd35732b3e62000d
Loic Coyle <loic.coyle@hotmail.fr>
2020-03-09 23:32:07 +0100

    redo Mpv with FileFetch

--- hotkey_helper/extractors/mpv.py
+++ hotkey_helper/extractors/mpv.py
@@ -1,37 +1,43 @@
 import re
+import os
+
+from pathlib import Path
 
 from .base import Extractor
 from .base import CommandCheck
-from .base import SectionExtract
-from .base import ManPageFetch
+from .base import FileFetch
+
 
 # need to add custom config bindings
 
-class Mpv(SectionExtract, ManPageFetch, CommandCheck, Extractor):
+class Mpv(FileFetch, CommandCheck, Extractor):
     cmd = 'mpv'
     has_modes = False
+    file_path = {'system': [Path('/usr/share/doc/mpv/input.conf'),
+                            Path('/etc/mpv/input.conf'),
+                            Path('/usr/local/etc/mpv/input.conf')],
+                 'user': [Path(os.environ['XDG_CONFIG_HOME']) / 'mpv/input.conf',
+                          Path(os.environ['HOME']) / '.config/mpv/input.conf']}
 
     @staticmethod
     def _clean_action(string):
-        string = string.replace('\\-', '-').replace('\n', ' ')
-        string = re.sub(r'(\\fB)|(\\fR)|(\.TP)|(\\fP)|(\.UNINDENT.*)|(\\&)|( ?\(?[Ss]ee .*?[\.\)])', '', string)
-        return string.rstrip()
+        return string
 
     @staticmethod
     def _clean_key(string):
-        string = re.sub(r'\\\(ga', '`', string)
-        string = re.sub(r'(\\\&?)', '', string)
-        string = re.sub(r' and ', ', ', string)
-        return string
+        return string[1:]
 
     def _extract(self):
-        ht_section = self.find_sections(self.fetched,
-                                        pattern='\.SS')['Keyboard Control']
-        keys = self.find_sections(ht_section, pattern='\.B')
         out = {}
-        for key, action in keys.items():
-            key = self._clean_key(key)
-            action = self._clean_action(action)
-            out[key] = action
+        line_ignore = re.compile(r'#\S')
+        line_remove = re.compile(r'#default-bindings.*')
+        for content in self.fetched.values():
+            content = re.sub(line_remove, '', content)
+            for line in content.split('\n'):
+                if re.match(line_ignore, line) :
+                    line_split = line.split()
+                    key = self._clean_key(line_split[0])
+                    action = self._clean_action(' '.join(line_split[1:]))
+                    out[key] = action
         return out
 
