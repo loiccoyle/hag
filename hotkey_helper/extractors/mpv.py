e5ec74ff84a576e288f0fca9b230f5e99717374d
Loic Coyle <loic.coyle@hotmail.fr>
2020-03-11 01:52:52 +0100

    convert mpv to sources

--- hotkey_helper/extractors/mpv.py
+++ hotkey_helper/extractors/mpv.py
@@ -6,18 +6,20 @@ from pathlib import Path
 from .base import Extractor
 from .base import CommandCheck
 from .base import FileFetch
+from .base import Command
+from .base import File
 
 
 # need to add custom config bindings
 
-class Mpv(FileFetch, CommandCheck, Extractor):
-    cmd = 'mpv'
+class Mpv(Extractor):
+    required = [Command('mpv')]
+    sources = {'system': [File('/usr/share/doc/mpv/input.conf'),
+                            File('/etc/mpv/input.conf'),
+                            File('/usr/local/etc/mpv/input.conf')],
+                 'user': [File(Path(os.environ['XDG_CONFIG_HOME']) / 'mpv/input.conf'),
+                          File(Path(os.environ['HOME']) / '.config/mpv/input.conf')]}
     has_modes = False
-    file_path = {'system': [Path('/usr/share/doc/mpv/input.conf'),
-                            Path('/etc/mpv/input.conf'),
-                            Path('/usr/local/etc/mpv/input.conf')],
-                 'user': [Path(os.environ['XDG_CONFIG_HOME']) / 'mpv/input.conf',
-                          Path(os.environ['HOME']) / '.config/mpv/input.conf']}
 
     @staticmethod
     def _clean_action(string):
@@ -32,12 +34,13 @@ class Mpv(FileFetch, CommandCheck, Extractor):
         line_ignore = re.compile(r'#\S')
         line_remove = re.compile(r'#default-bindings.*')
         for content in self.fetched.values():
-            content = re.sub(line_remove, '', content)
-            for line in content.split('\n'):
-                if re.match(line_ignore, line) :
-                    line_split = line.split()
-                    key = self._clean_key(line_split[0])
-                    action = self._clean_action(' '.join(line_split[1:]))
-                    out[key] = action
+            if content:
+                content = re.sub(line_remove, '', content[0])
+                for line in content.split('\n'):
+                    if re.match(line_ignore, line) :
+                        line_split = line.split()
+                        key = self._clean_key(line_split[0])
+                        action = self._clean_action(' '.join(line_split[1:]))
+                        out[key] = action
         return out
 
