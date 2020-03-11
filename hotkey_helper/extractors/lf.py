c54abbbcb4fbdf920752b9a4b07e64bf47ebc46c
Loic Coyle <loic.coyle@hotmail.fr>
2020-03-12 00:49:14 +0100

    improve regex

--- hotkey_helper/extractors/lf.py
+++ hotkey_helper/extractors/lf.py
@@ -13,34 +13,30 @@ class Lf(Extractor):
                'user': [File(Path(os.environ['XDG_CONFIG_HOME']) / 'lf' / 'lfrc')]}
     has_modes = False
 
-    @staticmethod
-    def _clean_key(string):
-        return string.replace("'", '').replace(')', '')
-
     def _extract(self):
-        line_match = re.compile(r"\s.*?((\(default ')|([^c]map))")
-        content_section = re.compile(r'Reference.*Configuration', re.DOTALL)
-        out = {}
+        content_clean = re.compile(r'.*?((boolean)|(string)|(integer)).*\n')
+        content_section = re.compile(r'Reference.*?Configuration', re.DOTALL)
+        default_key_action = re.compile(r'\s*(.*?)\s*\(default (.*)\)')
+        map_key_action = re.compile(r'\s*map\s(\w+)\s*(.*)\n')
+        content_clean_quote = re.compile(r'(?<!")\'(?!")')
         # extract the default bindings
-        fetched = re.search(content_section, self.fetched['default'][0])[0]
-        for line in fetched.split('\n'):
-            if re.match(line_match, line) and not 'string' in line:
-                line_split = line.split()
-                if line.lstrip().startswith('map'):
-                    key = line_split[1]
-                    action = ' '.join(line_split[2:])
-                else:
-                    key = self._clean_key(' '.join(line_split[2:]))
-                    action = line_split[0]
-                out[key] = action
+        # get the desired section
+        content = re.search(content_section, self.fetched['default'][0])[0]
+        # clean the problematic lines
+        content = re.sub(content_clean, '', content)
+        # remove the quotes could be improved
+        content = re.sub(content_clean_quote, '', content)
+        content = content.replace('\'"\'', '"'). replace('"\'"', '\'')
+        out = {}
+        for match in re.finditer(default_key_action, content):
+            out[match[2]] = match[1]
+
+        for match in re.finditer(map_key_action, content):
+            out[match[1]] = match[2]
 
         # extract the user bindings
         if self.fetched['user']:
-            user = self.fetched['user'][0]
-            for line in user.split('\n'):
-                if line.lstrip().startswith('map'):
-                    line_split = line.split()
-                    key = line_split[1]
-                    action = ' '.join(line_split[2:])
-                    out[key] = action
+            content = self.fetched['user'][0]
+            for match in re.finditer(map_key_action, content):
+                out[match[1]] = match[2]
         return out
